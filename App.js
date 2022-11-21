a4d184b7c35e8224b8144a9f91b7e9cf61961720
Rida Usman <ridausman7654@gmail.com>
2022-11-21 17:00:29 +0500

    pull req

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Just for pull request</Text>
+      <Text>Just for pull request.</Text>
       <StatusBar style="auto" />
     </View>
   );
