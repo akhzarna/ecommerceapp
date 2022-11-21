14987e97beb6a984ddeca912685c401f1f50aa4f
Muneeb <ahsan@vaionex.com>
2022-11-21 21:53:18 +0500

    fa20-bcs-030

--- App.js
+++ App.js
@@ -9,9 +9,9 @@ export default function App() {
   const [data, setData] = useState([
     {
       id: 1,
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 ",
       price: 109.95,
-      description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      description: "Your perfect pack for everyday use and  in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
       category: "men clothing",
       image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
     },
