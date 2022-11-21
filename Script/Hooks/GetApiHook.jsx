d7fc9c31e4abbf151933f0e4eae54b8ab426d49d
Ibrahim <mibrahim37612@gmail.com>
2022-11-21 16:25:17 +0500

    lab exam

---
+++ Script/Hooks/GetApiHook.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const getApiHook = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const getData = async (url, token = null) => {
+    await axios
+      .get(
+        url,
+        token
+          ? {
+              headers: {
+                Accept: "application/json",
+                Authorization: token,
+              },
+            }
+          : null
+      )
+      .then(function (response) {
+        /* console.log("Url is =", url);
+        console.log("Audio response is =", response.data);
+        console.log("Response is: ", response.status);
+        console.log("Auth token is: ", token); */
+        setError(response.data.error);
+        setData(response.data);
+        setLoading(false);
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    return data;
+  };
+
+  return { data, loading, error, getData };
+};
+
+export default getApiHook;
