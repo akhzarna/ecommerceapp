e31a51271337141694d52a8427d2f8c25047a5e3
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2022-11-21 16:20:07 +0500

    Card ui added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,22 @@
 import { View, Text, Image, Pressable } from "react-native";
-import React from "react";
+import { HeartIcon } from "react-native-heroicons/outline";
+import { HeartIcon as HeartFilled } from "react-native-heroicons/solid";
+import React, { useState } from "react";
 
 const Card = ({ item }) => {
+  const [liked, setliked] = useState(false);
   return (
     <View className="border items-center justify-center w-3/4rounded-md bg-white flex flex-col space-y-5">
+      <View>
+        {liked ? (
+          <HeartFilled
+            onClick={() => setliked(!liked)}
+            className="w-7 h-7 text-red-500"
+          />
+        ) : (
+          <HeartIcon onClick={() => setliked(!liked)} className="w-7 h-7" />
+        )}
+      </View>
       <Image
         style={{ width: "200", height: 200 }}
         source={{ uri: item?.image }}
