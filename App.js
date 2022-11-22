f833891bbd24450073addf4c9928c328d85e1c4a
Atif420 <fa20-bcs-051@cuilahore.edu.pk>
2022-11-22 11:07:41 +0500

    Lab Mid 051

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
+import { Feather } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Image,FlatList,StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useState,useEffect } from 'react';
+import 'localstorage-polyfill';
+import useFetch from './FetchReq';
 export default function App() {
+  const [data] = useFetch("https://fakestoreapi.com/products");
+  const [colorIcon,setColorIcon]=useState('black');
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(data));
+    console.log(data);
+  }, [data]);
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {console.log(data)}
+      <FlatList
+        data={data}
+        keyExtractor={({id},index) => id}
+        renderItem={({ item }) => ( 
+          <View style={{marginTop:10, borderStyle: 'solid'}}>
+          <TouchableOpacity
+          onPress={() => setColorIcon=='grey'}
+          >
+          </TouchableOpacity>
+          <Image
+          style={{width:200,height:200,alignSelf:'center'}}
+          source={{uri: item.image}}
+          >
+          </Image>
+          <Text style={{alignSelf:'center'}}>{item.title}</Text>
+          <Text style={{alignSelf:'center'}}>$ {item.price}</Text>
+          <Text style={{alignSelf:'center'}}>Buy Now!</Text>
+          {/* <Text> {item.id + '. ' + item.category }, {'Name: '+ item.title},{'USD: '+ item.price}</Text> */}
+          </View>
+        )}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +47,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50
   },
 });
