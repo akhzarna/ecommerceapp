986ab98437143a477add4117fa7b0cc5188b2c52
Asim Amir <101625565+Asimamir25@users.noreply.github.com>
2022-11-24 00:00:06 +0500

    Fetch hook

---
+++ fetchHook.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+import React from "react";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export default function useFetch(url) {
+  const [data, setdata] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setdata(res.data);
+        const stringifydata = JSON.stringify(res.data);
+        AsyncStorage.setItem("data", stringifydata);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return data;
+}
